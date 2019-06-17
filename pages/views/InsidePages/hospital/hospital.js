83a7b70b63ac9664f3993553e458c59378cf48a4
wangyupeng <wangyupeng@hesipay.com>
2019-06-17 16:40:17 +0800

    付款页面（完成）

--- pages/views/InsidePages/hospital/hospital.js
+++ pages/views/InsidePages/hospital/hospital.js
@@ -1,5 +1,11 @@
+const QQMapWX = require('../../../../utils/Map/qqmap-wx-jssdk')
+let qqmapsdk;
+const key = 'VN5BZ-7TCRI-OX7GZ-5KSG4-DKO27-QJBAH'
 Page({
     data: {
+        markers: [],
+        latitude: null,
+        longitude: null,
         Doctorlist: [{
             id: 1,
             name: '一甲医院 主治医师',
@@ -27,18 +33,68 @@ Page({
             selected: false
         }]
     },
-    onLoad () {},
-    onReady () {},
+    onLoad () {
+        qqmapsdk = new QQMapWX({key})
+    },
+    onReady () {
+        let {markers} = this.data
+        wx.getLocation({
+            type: 'wgs84',
+            altitude: true,
+            success: (respone) => {
+                const {latitude,longitude} = respone
+                qqmapsdk.search({
+                    keyword: 'kfc',
+                    location: `${latitude},${longitude}`,  //设置周边搜索中心点
+                    success: (respone) => {
+                        markers = respone.data.map(item => {
+                            return {
+                                id: item.id,
+                                title: item.title,
+                                latitude: item.location.lat,
+                                longitude: item.location.lng,
+                                iconPath: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTK84xyzRypsXnCeaFe6H8OACsEVBwibInL8afh6oHJGL3m7JhHgXdyO0jNO7SZMK2ExD0lxhjQD0EA/132',
+                                width: 50,
+                                height: 50,
+                                callout:{
+                                    content: `${item.title} \n地址：${item.address} \n电话：${item.tel}`,
+                                    color: '#0081ff',
+                                    padding: 10,
+                                    borderRadius: 6
+                                }
+                            }
+                        })
+                        this.setData({
+                            markers,
+                            latitude,
+                            longitude
+                        })
+                    },
+                    fail: (error) => {}
+                })
+            }
+        })
+    },
     onShow () {
         this.AnimationScale();
     },
     onUnload () {},
-    handleclick (params) {
+    /*handleclick (params) {
         const {id} = params.detail
         wx.navigateTo({
             url: '/pages/views/InsidePages/HospitalInfo/HospitalInfo'
         })
         console.log(params.detail)
+    },*/
+    lookhospital (params) {
+        const {markerId} = params
+        wx.navigateTo({
+            url: '/pages/views/InsidePages/HospitalInfo/HospitalInfo'
+        })
+        console.log(markerId)
+    },
+    backPrevPage () {
+        wx.navigateBack(-1)
     },
     // 按钮动效
     AnimationScale () {
