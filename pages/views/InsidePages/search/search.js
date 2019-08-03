009b17043c954136d5f8afdd6e587d26f62bc197
universe-peng <3464842637@qq.com>
2019-08-04 00:37:57 +0800

    搜索页面改版

--- pages/views/InsidePages/search/search.js
+++ pages/views/InsidePages/search/search.js
@@ -1,10 +1,37 @@
 Page({
-    data: {},
+    data: {
+        searchVal:'',
+        focus: false,
+        searchtype: 'drug'
+    },
     onLoad () {},
     onReady () {},
     onShow () {},
     onHide () {},
     onUnload () {},
+    // input获取焦点
+    searchInputfous () {
+        console.log('我获取焦点了========================')
+        this.setData({
+            focus: true
+        })
+    },
+    // 获取用户输入值
+    searchInputVal (ev) {
+        const {value} = ev.detail
+        this.setData({
+            searchVal: value
+        })
+        console.log(ev)
+    },
+    // 切换搜索类型
+    switchsearchtype (ev) {
+        const {searchType} = ev.currentTarget.dataset
+        this.setData({
+            searchtype: searchType
+        })
+    },
+    loadmore () {},
     // 分享
     onShareAppMessage () {
         return {
